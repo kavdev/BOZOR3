2cb57d7356802b8428b77602e9c90590a566eda2
Ian Dunn <iondune@gmail.com>
2014-05-21 12:54:09 -0700

    Add more structure

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
 
-var source = ['with', ['z','=',14], ['+', 'z', 'z']];
+function desugar_with(source) {
 
+}
 
 function interpret(source) {
-	switch (source[0]) {
-	case "with":
-		console.log("I'm a with!");
-		var a = interpret(source[1]);
-		var b = interpret(source[2]);
-		console.log("a and b are: " + a + ", " + b);
-		break;
-	default:
-		console.log("Confused.");
-		break;
-	}
+    /* if (_is string?!?!) {
+        // if (is number) else
+        switch
+        case "true"
+        case "false"
+
+        default
+            // this is an id or unbound
+    }*/
+    switch (source[0]) {
+    case "with":
+        code = desugar_with(source);
+        console.log("I'm a with!");
+        var a = interpret(source[1]);
+        var b = interpret(source[2]);
+        console.log("a and b are: " + a + ", " + b);
+        break;
+    case "fn":
+        break;
+    case "+":
+        break;
+    case "-":
+        break;
+    case "*":
+        break;
+    case "/":
+        break;
+    case "eq?":
+        break;
+    case "if":
+        break;
+    case "fn":
+        break;
+    default:
+        console.log("Confused.");
+        break;
+    }
+
+    return 0;
 }
+
+
+var source = ['with', ['z','=',14], ['+', 'z', 'z']];
 
 interpret(source);
