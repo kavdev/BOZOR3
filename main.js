a26133df0879eee2cd39523b1a5db115da41fab8
Ian Dunn <iondune@gmail.com>
2014-05-21 12:29:46 -0700

    Add initial work on intepret function

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+
+var source = ['with', ['z','=',14], ['+', 'z', 'z']];
+
+
+function interpret(source) {
+	switch (source[0]) {
+	case "with":
+		console.log("I'm a with!");
+		var a = interpret(source[1]);
+		var b = interpret(source[2]);
+		console.log("a and b are: " + a + ", " + b);
+		break;
+	default:
+		console.log("Confused.");
+		break;
+	}
+}
+
+interpret(source);
